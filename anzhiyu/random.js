18f15b5f6c2626ec17e1f10538df6408097ba116
sucaibaoku <agoniegirl@qq.com>
2024-04-25 17:01:33 +0800

    Site updated: 2024-04-25 17:01:33

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/25/hello-world/"];function toRandomPost(){
+var posts=["2024/04/25/hello-world/","2024/04/25/安装成功hexo博客/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
