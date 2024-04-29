5ea7c8951513b68e4725f91381d3bd79c878ffcd
sucaibaoku <agoniegirl@qq.com>
2024-04-29 14:52:36 +0800

    Site updated: 2024-04-29 14:52:35

---
+++ uptime/config.js
@@ -0,0 +1,33 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'Public Status',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur2452342-fe98c04f2ae2633f3561a535'
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Homepage',
+      url: 'https://status.org.cn/'
+    },
+    {
+      text: 'GitHub',
+      url: 'https://github.com/yb/uptime-status'
+    },
+    {
+      text: 'Blog',
+      url: 'https://abo.xyz/'
+    },
+  ],
+};
